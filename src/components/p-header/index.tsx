18eb2f3ee6117530e32f3ebb9c52f6b5647dd84f
burcuicen <burcuicen2000@gmail.com>
2024-02-12 00:03:16 +0300

    wip:header

--- src/components/p-header/index.tsx
+++ src/components/p-header/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './styles.scss';
+
 const PHeader: React.FC = () => {
     return (
-        <header className="header">
-            Header
+        <header className="p-header">
+            <div className='p-header__container'>
+                <img src={require('../../assets/logo.png')} alt="Pawtopia" className='p-header__logo' />
+                <div className='p-header__nav'>
+                        <a href="/">Home</a>
+                        <a href="/blog">Blog</a>
+                        <a href="/about">About</a>
+                        <a href="/contact">Contact</a>
+                </div>
+            </div>
         </header>
     );
 };
