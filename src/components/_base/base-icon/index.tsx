c28d2bcdda51c984e66d17b009c7c68caa50d828
burcuicen <burcuicen2000@gmail.com>
2024-02-15 23:34:33 +0300

    feat:header mobile view

--- src/components/_base/base-icon/index.tsx
+++ src/components/_base/base-icon/index.tsx
@@ -5,10 +5,11 @@ interface BaseIconProps {
     width?: number;
     height?: number;
     icon: string;
+    onClick?: () => void;
 }
-const BaseButton: React.FC<BaseIconProps> = ({ width, height, icon }) => {
+const BaseButton: React.FC<BaseIconProps> = ({ width, height, icon, onClick }) => {
     return (
-        <Icon icon={icon} width={width} height={height} />
+        <Icon icon={icon} width={width} height={height} onClick={onClick} />
     );
 }
 export default BaseButton;
--- src/components/p-header/index.tsx
+++ src/components/p-header/index.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-
 import { RootState } from '../../store/index';
-
 import './styles.scss';
-
 import BaseButton from '../_base/base-button';
 import BaseIcon from '../_base/base-icon';
 
 const PHeader: React.FC = () => {
     const isMobile = useSelector((state: RootState) => state.isMobile.value);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
 
     return (
         <header className="p-header">
             <div className='p-header__container'>
-                  <a href="/" className='p-header__title'>
-                  <img src={require('../../assets/logo.png')} alt="Pawtopia" className='p-header__logo' />
-                  </a>
-                  {!isMobile && (
+                <a href="/" className='p-header__title'>
+                    <img src={require('../../assets/logo.png')} alt="Pawtopia" className='p-header__logo' />
+                </a>
+                {!isMobile && (
                     <div className='p-header__nav'>
                         <a href="/">Home</a>
                         <a href="/blog">Blog</a>
@@ -27,11 +26,28 @@ const PHeader: React.FC = () => {
                     </div>
                 )}
             </div>
-            {!isMobile &&(<div className='p-header__actions'>
-                <BaseButton title='Login' type='default' />
-                <BaseButton title='Join' type='outline' />
-            </div>)}
-            {isMobile &&(<BaseIcon icon='material-symbols:menu' width={50} />)}
+            {!isMobile && (
+                <div className='p-header__actions'>
+                    <BaseButton title='Login' type='default' />
+                    <BaseButton title='Join' type='outline' />
+                </div>
+            )}
+            {isMobile && (
+                    <BaseIcon icon={dropdownOpen ? 'material-symbols:close':'material-symbols:menu'} width={36} onClick={toggleDropdown} />
+                )}
+            {isMobile && dropdownOpen && (
+                <div className='p-header__dropdown'>
+                    <a href="/" className='p-header__dropdown-item'>Home</a>
+                    <a href="/blog" className='p-header__dropdown-item'>Blog</a>
+                    <a href="/about" className='p-header__dropdown-item'>About</a>
+                    <a href="/contact" className='p-header__dropdown-item'>Contact</a>
+                    <div className='p-header__dropdown-actions'>
+                        <BaseButton title='Login' type='default' />
+                        <BaseButton title='Join' type='outline' />
+                    </div>
+                </div>
+                
+            )}
         </header>
     );
 };
