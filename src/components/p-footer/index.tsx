41fa1243921c47b0936d028756654cd81e28b910
burcuicen <burcuicen2000@gmail.com>
2024-02-17 01:06:23 +0300

    feat:add footer

--- src/components/p-footer/index.tsx
+++ src/components/p-footer/index.tsx
@@ -1,16 +1,40 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-
-import { RootState } from 'src/store';
 
 import './styles.scss';
 
+import BaseIcon from 'src/components/_base/base-icon';
+
 const PFooter: React.FC = () => {
-    const isMobile = useSelector((state: RootState) => state.isMobile.value);
 
     return (
         <footer className="p-footer">
-            {`Footer - Is Mobile: ${isMobile}`}
+            <div className="p-footer__container">
+                <div className='p-footer__head'>
+                        <div className='p-footer__nav'>
+                            <a href="/">Home</a>
+                            <a href="/blog">Blog</a>
+                            <a href="/about">About</a>
+                            <a href="/contact">Contact</a>
+                        </div>
+                        <div className='p-footer__social'>
+                            <BaseIcon icon='ic:baseline-facebook' width={24} />
+                            <BaseIcon icon='mdi:twitter' width={24} />
+                            <BaseIcon icon='mdi:instagram' width={24} />
+                        </div>
+                </div>
+                <div className='p-footer__bottom'>
+                        <div className='p-footer__copyright'>© 2024 Pawtopia. All rights reserved.</div>
+
+                    <div className='p-footer__logo'>
+                            <img src={require('../../assets/logo.png')} alt="Pawtopia" className='p-header__logo' />
+                    </div>
+                    <div className='p-footer__terms'>
+                            <a href='/terms'>Terms of Service</a>
+                            <a href='/privacy'>Privacy Policy</a>
+                    </div>
+
+                </div>
+            </div>
         </footer>
     );
 };
