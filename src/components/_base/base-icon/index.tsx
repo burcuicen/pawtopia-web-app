2d0a507f41409d746220e3d932b226e26255c80e
burcuicen <burcuicen2000@gmail.com>
2024-02-24 20:06:33 +0300

    feat:add stepper component

--- src/components/_base/base-icon/index.tsx
+++ src/components/_base/base-icon/index.tsx
@@ -5,11 +5,12 @@ interface BaseIconProps {
     width?: number;
     height?: number;
     icon: string;
+    className?: string;
     onClick?: () => void;
 }
-const BaseButton: React.FC<BaseIconProps> = ({ width, height, icon, onClick }) => {
+const BaseButton: React.FC<BaseIconProps> = ({ width, height, icon, onClick, className }) => {
     return (
-        <Icon icon={icon} width={width} height={height} onClick={onClick} />
+        <Icon icon={icon} width={width} height={height} onClick={onClick} className={className} />
     );
 }
 export default BaseButton;
--- src/pages/survey/index.tsx
+++ src/pages/survey/index.tsx
@@ -1,8 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useState } from 'react';
+import PStepper from 'src/components/p-stepper';
 
 const SurveyPage: React.FC = () => {
+    const totalSteps = 5;
+  const [activeStep, setActiveStep] = useState(1);
+
+  const handleStepChange = (step:number) => {
+    setActiveStep(step);
+  };
+
     return (
         <div className="page page__survey">
+             <PStepper
+        steps={totalSteps}
+        activeStep={activeStep}
+        onStepChange={handleStepChange}
+      />
         </div>
     );
 };
