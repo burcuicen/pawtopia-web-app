a1e0b6817892765b4bd114cdd4bdfbdfdf0ac533
burcuicen <burcuicen2000@gmail.com>
2024-07-20 16:57:01 +0300

    fix:syntax with prettier

--- src/api/interfaces/user.ts
+++ src/api/interfaces/user.ts
@@ -1,54 +1,54 @@
-import { IBase } from "./base";
+import { IBase } from './base'
 
 interface ISurveyResult {
-  purpose?: "looking-pet" | "looking-guardian" | "other";
-  animalPreference?: "cat" | "dog" | "other" | "just-looking";
-  ageRange?: "baby" | "adult" | "senior";
-  genderPreference?: "male" | "female";
-  healthStatus?: "healthy" | "special-needs";
-  animalCareHistory?: boolean;
-  reason?: string;
+  purpose?: 'looking-pet' | 'looking-guardian' | 'other'
+  animalPreference?: 'cat' | 'dog' | 'other' | 'just-looking'
+  ageRange?: 'baby' | 'adult' | 'senior'
+  genderPreference?: 'male' | 'female'
+  healthStatus?: 'healthy' | 'special-needs'
+  animalCareHistory?: boolean
+  reason?: string
 }
 interface IUser extends IBase {
-  username: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  userType: "paw-seeker" | "paw-guardian" | "other" | "paw-admin";
-  surveyResults?: ISurveyResult;
-  country: string;
-  city: string;
+  username: string
+  email: string
+  password: string
+  firstName: string
+  lastName: string
+  userType: 'paw-seeker' | 'paw-guardian' | 'other' | 'paw-admin'
+  surveyResults?: ISurveyResult
+  country: string
+  city: string
 }
 interface IUserCreateDto {
-  username: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  userType: "paw-seeker" | "paw-guardian" | "other";
-  surveyResults?: ISurveyResult;
-  country: string;
-  city: string;
+  username: string
+  email: string
+  password: string
+  firstName: string
+  lastName: string
+  userType: 'paw-seeker' | 'paw-guardian' | 'other'
+  surveyResults?: ISurveyResult
+  country: string
+  city: string
 }
 export interface ILogin {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 
 export interface ILoginResponse {
-  token: string;
+  token: string
 }
 
 export interface IRegisterDto {
-  username: string;
-  email: string;
-  password: string;
-  firstName?: string;
-  lastName?: string;
-  userType?: "paw-seeker" | "paw-guardian" | "other";
-  surveyResults?: ISurveyResult;
-  country: string;
-  city: string;
+  username: string
+  email: string
+  password: string
+  firstName?: string
+  lastName?: string
+  userType?: 'paw-seeker' | 'paw-guardian' | 'other'
+  surveyResults?: ISurveyResult
+  country: string
+  city: string
 }
-export type { IUser, IUserCreateDto, ISurveyResult };
+export type { IUser, IUserCreateDto, ISurveyResult }
