2eab8c80f3061a490f624159f42d093065e0899a
burcuicen <burcuicen2000@gmail.com>
2024-07-20 18:29:31 +0300

    feat:create landing page hero

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 import './styles.scss'
+import BaseButton from 'src/components/_base/base-button'
 const Landing: React.FC = () => {
-  return <div className="landing"></div>
+  return (
+    <div className="landing">
+      <div className="landing__hero">
+        <div className="landing__hero-content">
+          <div className="landing__hero-title">
+            <div className="landing__hero-title landing__hero-title-1">One More Friend</div>
+            <div className="landing__hero-title landing__hero-title-2">Thousands more fun!</div>
+          </div>
+          <div className="landing__hero-subtitle">
+            Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets
+            that can meet your needs!
+          </div>
+          <div className="landing__hero-actions">
+            <BaseButton title="View Intro" type="outline" />
+            <BaseButton title="Explore Now" type="default" />
+          </div>
+        </div>
+        <div className="landing__hero-image">
+          <img src={require('../../assets/hero-img.png')} alt="" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Landing
