b2e45cdf3ea863264ee698878e17841411eaacea
burcuicen <burcuicen2000@gmail.com>
2024-07-20 16:56:35 +0300

    fix:syntax with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,48 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
-import { setIsMobile } from 'src/store/reducers/isMobileSlice';
-import { useCheckLoginStatus } from 'src/helpers/auth';
+import { setIsMobile } from 'src/store/reducers/isMobileSlice'
+import { useCheckLoginStatus } from 'src/helpers/auth'
 
-import MainLayout from 'src/layouts/main';
-import Landing from 'src/pages/landing';
-import Login from 'src/pages/login';
-import Logout from './pages/logout';
-import Signup from 'src/pages/signup';
-import Survey from 'src/pages/survey';
+import MainLayout from 'src/layouts/main'
+import Landing from 'src/pages/landing'
+import Login from 'src/pages/login'
+import Logout from './pages/logout'
+import Signup from 'src/pages/signup'
+import Survey from 'src/pages/survey'
 
 const App: React.FC = () => {
-  useCheckLoginStatus();
+  useCheckLoginStatus()
 
-  const dispatch = useDispatch();
-  
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const handleResize = () => {
-      const mobile = window.innerWidth < 768;
-      dispatch(setIsMobile(mobile));
-    };
+      const mobile = window.innerWidth < 768
+      dispatch(setIsMobile(mobile))
+    }
 
-    window.addEventListener('resize', handleResize);
-    handleResize();
+    window.addEventListener('resize', handleResize)
+    handleResize()
 
     return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [dispatch]);
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [dispatch])
   return (
     <Router>
       <MainLayout>
         <Routes>
           <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/logout" element={<Logout/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/onboarding" element={<Survey />} />
         </Routes>
       </MainLayout>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
