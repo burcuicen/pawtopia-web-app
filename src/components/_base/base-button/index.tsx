375b40ab74b02d4e3043701b296c9b59387368af
burcuicen <burcuicen2000@gmail.com>
2024-07-20 16:57:11 +0300

    fix:syntax with prettier

--- src/components/_base/base-button/index.tsx
+++ src/components/_base/base-button/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import './styles.scss';
+import React from 'react'
+import './styles.scss'
 
 interface BaseButtonProps {
-    title: string;
-    type: 'default' | 'outline'
-    size?: 'small' | 'medium' | 'large';
-    rounded?: boolean;
-    onClick?: () => void;
+  title: string
+  type: 'default' | 'outline'
+  size?: 'small' | 'medium' | 'large'
+  rounded?: boolean
+  onClick?: () => void
 }
 const BaseButton: React.FC<BaseButtonProps> = ({ title, type, size, onClick, rounded }) => {
-    return (
-        <button className={`base-button base-button--${type} base-button--${size} base-button--${rounded ? 'rounded' : ''}`} onClick={onClick}>{title}</button>
-    );
+  return (
+    <button className={`base-button base-button--${type} base-button--${size} base-button--${rounded ? 'rounded' : ''}`} onClick={onClick}>
+      {title}
+    </button>
+  )
 }
-export default BaseButton;
+export default BaseButton
