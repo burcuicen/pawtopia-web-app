280bc4c8aa4685e18b1e1353be9829ef9d420f03
burcuicen <burcuicen2000@gmail.com>
2024-07-21 14:07:18 +0300

    feat:change hero img on template

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -6,22 +6,24 @@ const Landing: React.FC = () => {
   return (
     <div className="landing">
       <div className="landing__hero">
-        <div className="landing__hero-content">
-          <div className="landing__hero-title">
-            <div className="landing__hero-title landing__hero-title-1">One More Friend</div>
-            <div className="landing__hero-title landing__hero-title-2">Thousands more fun!</div>
+        <div className="landing__hero-container">
+          <div className="landing__hero-content">
+            <div className="landing__hero-title">
+              <div className="landing__hero-title landing__hero-title-1">One More Friend</div>
+              <div className="landing__hero-title landing__hero-title-2">Thousands more fun!</div>
+            </div>
+            <div className="landing__hero-subtitle">
+              Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets
+              that can meet your needs!
+            </div>
+            <div className="landing__hero-actions">
+              <BaseButton title="View Intro" type="outline" />
+              <BaseButton title="Explore Now" type="default" />
+            </div>
           </div>
-          <div className="landing__hero-subtitle">
-            Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets
-            that can meet your needs!
+          <div className="landing__hero-image-container">
+            <img className="landing__hero-image" src={require('../../assets/hero-img.png')} alt="Woman holding a cat" />
           </div>
-          <div className="landing__hero-actions">
-            <BaseButton title="View Intro" type="outline" />
-            <BaseButton title="Explore Now" type="default" />
-          </div>
-        </div>
-        <div className="landing__hero-image">
-          <img src={require('../../assets/hero-img.png')} alt="" />
         </div>
       </div>
     </div>
